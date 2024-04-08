ec62ef1c481fa0f03c76b3fd1226a7c2ca953b5d
alkaz07 <alkaz07@yandex.ru>
2024-04-08 17:18:45 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        example2();
-        example3();
-        example4();
+//        example2();
+//        example3();
+//        example4();
+        task1();
     }
 
     private static void example2() {
@@ -77,5 +78,50 @@ public class Main {
         //сортировка
         strings.sort(String::compareTo);
         System.out.println(strings);
+    }
+
+    public static ArrayList<Integer> inputPositiveList(){
+        //Создать список целых чисел
+        ArrayList<Integer> nums = new ArrayList<>();
+        //Попросить пользователя вводить только положительные числа.
+        System.out.println("Вводите положительные числа");
+        //Как только он введет отрицательное или 0, закончить ввод
+        //
+        Scanner scanner= new Scanner(System.in);
+//        int x = scanner.nextInt();
+//        while (x>0)
+//        {
+//            nums.add(x);
+//            x = scanner.nextInt();
+//        }
+        for(int x = scanner.nextInt() ; x>0 ; x = scanner.nextInt())
+            nums.add(x);
+        return nums;
+    }
+
+    public static void task1(){
+        ArrayList<Integer> nums = inputPositiveList();
+        //Вывести список введенных чисел
+        System.out.println(nums);
+        //Вывести этот же список, отсортированный по возрастанию
+
+        //Вычислить медианное значение для введенного списка
+        double med = mediana(nums);
+        System.out.println(med);
+        nums.sort(Comparator.naturalOrder());
+        System.out.println(nums);
+    }
+
+    private static double mediana(ArrayList<Integer> nums) {
+        nums.sort(Comparator.naturalOrder());
+        int len = nums.size();
+        if(len %2 == 1)
+            return nums.get(len/2);
+        else
+        {
+            int a = nums.get(len/2);
+            int b = nums.get(len/2 - 1);
+            return (a+b) / 2.0;
+        }
     }
 }
